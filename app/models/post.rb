efe732bb477c726aedb8c034940e0efb7f8d9656
Rob Romaniw <robertromaniw@gmail.com>
2016-11-14 12:24:57 -0500

    Paperclip gem scrapped | Files will be linked via URL

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,7 @@ class Post < ApplicationRecord
   acts_as_votable
   belongs_to :user
   has_many :comments, dependent: :destroy
-  has_attached_file :image,
-  path: ":rails_root/public/system/:attachment/:id/:style/:filename",
-  url: "/system/:attachment/:id/:style/:filename",
-  styles: { medium: "700x500#>", small: "350x250>" }
+  has_attached_file :image, styles: { medium: "700x500#>", small: "350x250>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
