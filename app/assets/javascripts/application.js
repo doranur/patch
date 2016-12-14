837e95d9f5665ab0f00f0260a0a1a2628068bc90
Rob Romaniw <robertromaniw@gmail.com>
2016-12-14 09:40:02 -0500

    fixing logo

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,17 +16,17 @@
 
 
 
-$(document).ready(function() {
-
-  $(".closed-box").mouseenter(function() {
-      $(".open-box").show();
-      $(".closed-box").hide();
-      $(this).hide();
-  });
-
-  $(".open-box").mouseout(function() {
-    $(".closed-box").show();
-    $(".open-box").hide();
-    $(this).hide();
-  });
-});
+// $(document).ready(function() {
+//
+//   $(".closed-box").mouseenter(function() {
+//       $(".open-box").show();
+//       $(".closed-box").hide();
+//       $(this).hide();
+//   });
+//
+//   $(".open-box").mouseout(function() {
+//     $(".closed-box").show();
+//     $(".open-box").hide();
+//     $(this).hide();
+//   });
+// });
